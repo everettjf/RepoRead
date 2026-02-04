84304c74d4a3ce06c9a0efe6529efbf4f75a6866
everettjf <everettjf@live.com>
2026-02-04 09:10:26 -0800

    new version: 1.1.0

---
+++ inc_minor_version.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Get current version from package.json
+CURRENT_VERSION=$(grep '"version"' package.json | head -1 | sed 's/.*"version": "\([^"]*\)".*/\1/')
+
+# Parse version components
+IFS='.' read -r MAJOR MINOR PATCH <<< "$CURRENT_VERSION"
+
+# Increment minor, reset patch
+NEW_MINOR=$((MINOR + 1))
+NEW_VERSION="$MAJOR.$NEW_MINOR.0"
+
+echo "Bumping version: $CURRENT_VERSION -> $NEW_VERSION"
+
+# Update package.json
+sed -i '' "s/\"version\": \"$CURRENT_VERSION\"/\"version\": \"$NEW_VERSION\"/" package.json
+
+# Update Cargo.toml
+sed -i '' "s/^version = \"$CURRENT_VERSION\"/version = \"$NEW_VERSION\"/" src-tauri/Cargo.toml
+
+# Update tauri.conf.json
+sed -i '' "s/\"version\": \"$CURRENT_VERSION\"/\"version\": \"$NEW_VERSION\"/" src-tauri/tauri.conf.json
+
+# Add tag to git
+git add .
+git commit -m "new version: $NEW_VERSION"
+git push
+git tag $NEW_VERSION
+git push --tags
+
+echo "Done!"
